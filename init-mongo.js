1555f1a4c4cad2a1dbc59e30a652aa0f7f1a579b
tad <Rhoster@Protonmail.com>
2022-10-29 17:02:05 -0500

    Adding new Collections

--- init-mongo.js
+++ init-mongo.js
@@ -1,20 +1,71 @@
 db = new Mongo().getDB("admin");
-db.auth('admin', 'password');
-db.createUser(
+db.auth("admin", "password");
+db.createUser({
+  user: "app",
+  pwd: "password",
+  roles: [
     {
-        user: "app",
-        pwd: "password",
-        roles: [
-            {
-                role: "readWrite",
-                db: "gims"
-            }
-        ]
-    }
-);
-db = db.getSiblingDB('gims');
-db.createCollection('items');
-db.items.insertOne({"name": "Test Name", "category": "Test Category", "type": "Test Type", "brand": "Test Brand", "description": "This is a test description", "imageUrl": null});
-db.items.insertOne({"name": "Widget A", "category": "random", "type": "component", "brand": "Acme", "description": "This is a description of Widget A.", "imageUrl": null});
-db.items.insertOne({"name": "Widget B", "category": "random", "type": "component", "brand": "Acme", "description": "This is a description of Widget B.", "imageUrl": null});
-db.items.insertOne({"name": "Widget C", "category": "random", "type": "component", "brand": "Acme", "description": "This is a description of Widget C.", "imageUrl": null});
+      role: "readWrite",
+      db: "gims",
+    },
+  ],
+});
+db = db.getSiblingDB("gims");
+
+//////////////////////////////////////////////////////////
+// Categories
+//////////////////////////////////////////////////////////
+db.createCollection("categories");
+db.categories.insertOne({ _id: NumberInt(1), name: "Electronics" });
+db.categories.insertOne({ _id: NumberInt(2), name: "Plumbing" });
+db.categories.insertOne({ _id: NumberInt(3), name: "HVAC" });
+
+//////////////////////////////////////////////////////////
+// Types
+//////////////////////////////////////////////////////////
+db.createCollection("types");
+db.types.insertOne({ _id: NumberInt(1), name: "Work in Progress", consumable: "false" });
+db.types.insertOne({ _id: NumberInt(2), name: "Finished Goods", consumable: "false" });
+db.types.insertOne({ _id: NumberInt(3), name: "Component", consumable: "false" });
+db.types.insertOne({ _id: NumberInt(4), name: "Raw Materials", consumable: "false" });
+db.types.insertOne({ _id: NumberInt(5), name: "Packing and Packaging", consumable: "true" });
+db.types.insertOne({ _id: NumberInt(6), name: "Cycle Inventory", consumable: "true" });
+db.types.insertOne({ _id: NumberInt(7), name: "Excess Inventory (Obsolete)", consumable: "false"});
+db.types.insertOne({ _id: NumberInt(8), name: "Transit (Incoming Inventory)", consumable: "false"});
+
+//////////////////////////////////////////////////////////
+// Inventory
+//////////////////////////////////////////////////////////
+db.createCollection("items");
+db.items.insertOne({
+  name: "Test Name",
+  category: 1,
+  type: 2,
+  brand: "Test Brand",
+  description: "This is a test description",
+  imageUrl: null,
+});
+db.items.insertOne({
+  name: "Widget A",
+  category: 2,
+  type: 3,
+  brand: "Acme",
+  description: "This is a description of Widget A.",
+  imageUrl: null,
+});
+db.items.insertOne({
+  name: "Widget B",
+  category: 2,
+  type: 3,
+  brand: "Acme",
+  description: "This is a description of Widget B.",
+  imageUrl: null,
+});
+db.items.insertOne({
+  name: "Widget C",
+  category: 2,
+  type: 3,
+  brand: "Acme",
+  description: "This is a description of Widget C.",
+  imageUrl: null,
+});
