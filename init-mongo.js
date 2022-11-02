f13e9db911dbf2fb11bc10520eb7e271ff4ff8af
tad <Rhoster@Protonmail.com>
2022-11-01 19:00:17 -0500

    An alternative to how to normalize

--- init-mongo.js
+++ init-mongo.js
@@ -16,22 +16,22 @@ db = db.getSiblingDB("gims");
 // Categories
 //////////////////////////////////////////////////////////
 db.createCollection("categories");
-db.categories.insertOne({ _id: NumberInt(1), name: "Electronics" });
-db.categories.insertOne({ _id: NumberInt(2), name: "Plumbing" });
-db.categories.insertOne({ _id: NumberInt(3), name: "HVAC" });
+db.categories.insertOne({ _id: "Electronics" });
+db.categories.insertOne({ _id: "Plumbing" });
+db.categories.insertOne({ _id: "HVAC" });
 
 //////////////////////////////////////////////////////////
 // Types
 //////////////////////////////////////////////////////////
 db.createCollection("types");
-db.types.insertOne({ _id: NumberInt(1), name: "Work in Progress", consumable: "false" });
-db.types.insertOne({ _id: NumberInt(2), name: "Finished Goods", consumable: "false" });
-db.types.insertOne({ _id: NumberInt(3), name: "Component", consumable: "false" });
-db.types.insertOne({ _id: NumberInt(4), name: "Raw Materials", consumable: "false" });
-db.types.insertOne({ _id: NumberInt(5), name: "Packing and Packaging", consumable: "true" });
-db.types.insertOne({ _id: NumberInt(6), name: "Cycle Inventory", consumable: "true" });
-db.types.insertOne({ _id: NumberInt(7), name: "Excess Inventory (Obsolete)", consumable: "false"});
-db.types.insertOne({ _id: NumberInt(8), name: "Transit (Incoming Inventory)", consumable: "false"});
+db.types.insertOne({ _id: "Work in Progress", consumable: "false" });
+db.types.insertOne({ _id: "Finished Goods", consumable: "false" });
+db.types.insertOne({ _id: "Component", consumable: "false" });
+db.types.insertOne({ _id: "Raw Materials", consumable: "false" });
+db.types.insertOne({ _id: "Packing and Packaging", consumable: "true" });
+db.types.insertOne({ _id: "Cycle Inventory", consumable: "true" });
+db.types.insertOne({ _id: "Excess Inventory (Obsolete)", consumable: "false"});
+db.types.insertOne({ _id: "Transit (Incoming Inventory)", consumable: "false"});
 
 //////////////////////////////////////////////////////////
 // Inventory
@@ -39,32 +39,32 @@ db.types.insertOne({ _id: NumberInt(8), name: "Transit (Incoming Inventory)", co
 db.createCollection("items");
 db.items.insertOne({
   name: "Test Name",
-  category: 1,
-  type: 2,
+  category: "Electronics",
+  type: "Finished Goods",
   brand: "Test Brand",
   description: "This is a test description",
   imageUrl: null,
 });
 db.items.insertOne({
   name: "Widget A",
-  category: 2,
-  type: 3,
+  category: "Plumbing",
+  type: "Component",
   brand: "Acme",
   description: "This is a description of Widget A.",
   imageUrl: null,
 });
 db.items.insertOne({
   name: "Widget B",
-  category: 2,
-  type: 3,
+  category: "Plumbing",
+  type: "Component",
   brand: "Acme",
   description: "This is a description of Widget B.",
   imageUrl: null,
 });
 db.items.insertOne({
   name: "Widget C",
-  category: 2,
-  type: 3,
+  category: "Plumbing",
+  type: "Component",
   brand: "Acme",
   description: "This is a description of Widget C.",
   imageUrl: null,
